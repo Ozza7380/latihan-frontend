62fc3f2ebf62d21deb57ce5d0745398ef27cb335
M Raihan H <ar937464@gmail.com>
2025-12-27 12:25:21 +0700

    menghapus inpu wa

--- wa.js
+++ wa.js
@@ -4,22 +4,22 @@ form.addEventListener('submit', function (event) { // form di cek ketika mengcli
     event.preventDefault(); // menghilangkan event bawaan seperti reload ketika di pencet
 
     const name = document.getElementById("name").value;
-    const whatsapp = document.getElementById("WhatsApp").value;
+   // const whatsapp = document.getElementById("WhatsApp").value;
     const alamat = document.getElementById("Alamat").value;
     const barang = document.getElementById("barang").value;
     console.log("Nama:", name)
-    console.log("WhatsApp:", whatsapp); 
+    //console.log("WhatsApp:", whatsapp); 
     console.log("alamat:", alamat)
     console.log("barang pesanan:", barang)
-    if (!whatsapp.startsWith("62")) {
-        alert("No  WhatsApp harus di awali 62!");
-        return;
-    }
+    //if (!whatsapp.startsWith("62")) {
+    //  alert("No  WhatsApp harus di awali 62!");
+    //    return;
+    // }
     console.log("wa sesuai")
-    const massage = ` assalamualaikum Admin, 
-saya ingin melakukan pesanan atas nama *${name.trim()}*
-dengan alamat *${alamat.trim()}*
-dan pesanan sebagai berikut:
+    const massage = ` Assalamualaikum Admin, 
+Saya ingin melakukan pesanan atas nama *${name.trim()}*
+Dengan alamat *${alamat.trim()}*
+Dan pesanan sebagai berikut:
 *${barang.trim()}*
 
 terimakasih.`;
