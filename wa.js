d61e7d5c9186ccb1622e5caf4e11aae4c91c2e44
M Raihan H <ar937464@gmail.com>
2025-12-27 12:06:24 +0700

    menambahkan alamat, pesanan

--- wa.js
+++ wa.js
@@ -5,15 +5,24 @@ form.addEventListener('submit', function (event) { // form di cek ketika mengcli
 
     const name = document.getElementById("name").value;
     const whatsapp = document.getElementById("WhatsApp").value;
+    const alamat = document.getElementById("Alamat").value;
+    const barang = document.getElementById("barang").value;
     console.log("Nama:", name)
     console.log("WhatsApp:", whatsapp); 
+    console.log("alamat:", alamat)
+    console.log("barang pesanan:", barang)
     if (!whatsapp.startsWith("62")) {
         alert("No  WhatsApp harus di awali 62!");
         return;
     }
     console.log("wa sesuai")
-    const massage = `assalamualaikum Admin, 
-    saya ingin melakukan registrasi atas nama ${name} dan no wa ${whatsapp}, terimakasih.`;
+    const massage = ` assalamualaikum Admin, 
+saya ingin melakukan pesanan atas nama *${name.trim()}*
+dengan alamat *${alamat.trim()}*
+dan pesanan sebagai berikut:
+*${barang.trim()}*
+
+terimakasih.`;
     console.log("peasan:", massage)
     const endcodeMassage = encodeURIComponent(massage);
     console.log("encoded:", endcodeMassage)
